b0f913d6cf81c4a5026024d0dcf0b6783e9fbc96
Yuto Matsuo <yutom281@gmail.com>
2020-09-20 12:53:53 +0900

    final

--- Sudoku.java
+++ Sudoku.java
@@ -7,20 +7,28 @@ public class Sudoku {
 	static private Field field;
 
 	public enum Difficulty {
-		Easy(200),
-		Normal(350),
-		Hard(500),
-		Expert(650),
-		Impossible(100000);
-		
+
+		Easy(270),
+		e(270),
+		Normal(280),
+		n(280),
+		Hard(290),
+		h(290),
+		Professional(300),
+		p(300),
+		Impossible(100000),
+		i(100000);
+
 		private int threshold;
 
 		private Difficulty(int threshold) {
 			this.threshold = threshold;
 		}
+
 		int get() {
 			return threshold;
 		}
+
 		static boolean checkIfExist(String input) {
 			for(Difficulty dif: values()) {
 				if(dif.name().equals(input)) {
@@ -30,17 +38,17 @@ public class Sudoku {
 			System.out.println("正しい値を入力してください");
 			return false;
 		}
+
 	};
 
 	public static void main(String[] args) {
-
 		// 入力
 		if(args.length == 0) {
 			Scanner scan = new Scanner(System.in);
 			String input;
 			do {
 				System.out.println("難易度を選択してください");
-				System.out.println("[ Easy Normal Hard Expert Impossible ]");
+				System.out.println("[ Easy Normal Hard Professional Impossible ]");
 				input = scan.next();
 			} while(! Difficulty.checkIfExist(input));
 
@@ -57,30 +65,23 @@ public class Sudoku {
 			field.init();
 		}
 
-		// 検査
+		// 問題の検査
 		try {
-			field.inspect();
-			
+			field.inspect(true);
 		} catch(InputException ie) {
 			ie.printStackTrace();
 			System.exit(10);
 		}
 
 		// 解答
-		while(! field.changeMode()) {
+		while(! field.isStuck()) {
 			field.run();
 		}
-		while(field.check()) {
+		while(! field.isSolved()) {
 			field.solver();
 		}
 
 		// 出力
-		if(! field.check()) {
-			field.output();
-		} else {
-			field.debug();
-			System.out.println("解答できませんでした。途中経過を出力します");
-		}
-
+		field.output();
 	}
 }
